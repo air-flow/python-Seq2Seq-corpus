ee1664004c6f0428bbcfd9c876bbefe7cd7b80d1
辻気流 <kiryu.18011106@gmail.com>
2020-11-09 15:07:07 +0900

    余計な文章削除し、メッセージの冒頭識別子削除文章完成

--- all.py
+++ all.py
@@ -4,12 +4,12 @@ path = 'C:\\Users\\kiryu\\Desktop\\tuji\\textdata\\nucc\\'
 
 def DataText(path):
     result_list = []
-    with open(path+'data004.txt',"r",encoding="utf-8") as f:
+    with open(path+'data005.txt',"r",encoding="utf-8") as f:
         l = f.readlines()
     return l
 
 def AttoMarkDelete(data):
-    result = [i for i in data if i[0] != '＠']
+    result = [i for i in data if i[0] not in  ['＠','％']]
     return result
 
 
@@ -19,13 +19,39 @@ def MatchMember(parameter_list):
         return True
     return False
 
+def BracketHittingEachOtherDeletion(text):
+    return re.sub("[（）]", "", text)
+
+def BracketMovementDeletion(text):
+    return re.sub("[＜＞]", "", text)
+
+def BracketFuriganaDeletion(text):
+    return re.sub("[【】]", "", text)
+
+
+def DeleteTextOpesion(text):
+    # hitting_each_other （）delete （うん）（ふーん）
+    # movement ＜＞ delete ＜録音中断＞ ＜笑い＞
+    # furigana 【】 delete 【留学生の名前】
+    # all movement and hitting_each_other and furigana delete
+    hitting_each_other = BracketHittingEachOtherDeletion(text)
+    movement = BracketMovementDeletion(text)
+    furigana = BracketFuriganaDeletion(text)
+    all =  BracketFuriganaDeletion(BracketMovementDeletion(hitting_each_other))
+    return all
+
+def NameDelete(text):
+    index = text.find("：")
+    return text[index+1:]
+
 def UnionText(data):
     index = 0
     result = []
     while index < len(data):
-        temp = data[index].rstrip("\n")
+        temp = DeleteTextOpesion(data[index].rstrip("\n"))
         if MatchMember(temp):
-            result.append(temp)
+            name = NameDelete(temp)
+            result.append(name)
         else:
             result[-1] = result[-1]+temp
         index+=1
