1db24d97dd32cebc6e0d2dc772c98df861c89a27
辻気流 <kiryu.18011106@gmail.com>
2020-11-09 10:57:43 +0900

    複数行に渡る対話を一つにまとめる

--- all.py
+++ all.py
@@ -1,5 +1,5 @@
 import pprint
-
+import re
 path = 'C:\\Users\\kiryu\\Desktop\\tuji\\textdata\\nucc\\'
 
 def DataText(path):
@@ -12,8 +12,27 @@ def AttoMarkDelete(data):
     result = [i for i in data if i[0] != '＠']
     return result
 
+
+def MatchMember(parameter_list):
+    m = re.match(r'[a-zA-Z]+[0-9]+', parameter_list)
+    if m != None:
+        return True
+    return False
+
+def UnionText(data):
+    index = 0
+    result = []
+    while index < len(data):
+        temp = data[index].rstrip("\n")
+        if MatchMember(temp):
+            result.append(temp)
+        else:
+            result[-1] = result[-1]+temp
+        index+=1
+    return result
 if __name__ == "__main__":
     data = DataText(path)
-    pprint.pprint(len(data))
-    print(len(AttoMarkDelete(data)))
-    
+    # pprint.pprint(ldata))
+    user_text = AttoMarkDelete(data)
+    # pprint.pprint(AttoMarkDelete(data))
+    pprint.pprint(UnionText(user_text))
