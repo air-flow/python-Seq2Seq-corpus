16f4193a29a377759676660d13affe55bf12be31
辻気流 <kiryu.18011106@gmail.com>
2020-12-16 16:17:50 +0900

    読み込み整形完成

--- screen.py
+++ screen.py
@@ -1,5 +1,33 @@
 import pprint
 
-with open('./mine/scenario/8.txt',encoding='shift-js',mode='r') as target:
-    l = target.readlines()
-    pprint.pprint(l)
+def GetFileContens(file_name):
+    l = None
+    with open('./mine/scenario/'+file_name,encoding='utf-8',mode='r') as target:
+        l = target.readlines()
+        l = list(map(lambda x: x.replace( '\n' , '' ),l))
+    return l
+
+def SetFileContens(l,file_name):
+    l = None
+    with open('./mine/data/scenario/input/'+file_name,encoding='utf-8',mode='x') as target:
+        target.writelines(l)
+        # l = list(map(lambda x: x.replace( '\n' , '' ),l))
+    return l
+
+def main():
+    input/
+
+def DeleteParentheses(text):
+    if "」" in text:
+        i = text.find("「")+1
+        j = text.index("」")
+        return text[i:j]
+    else:
+        return False
+if __name__ == "__main__":
+    file_name = "8uft8.txt"
+    file = GetFileContens(file_name)
+    text_list = list(map(DeleteParentheses,file))
+    # pprint.pprint(text_list)
+    # pprint.pprint(list(map(DeleteParentheses,file)))
+    # print(DeleteParentheses(file))
