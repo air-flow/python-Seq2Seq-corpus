0d75834ebdfa2463e9f78f99e92db1e9870a91fd
辻気流 <kiryu.18011106@gmail.com>
2020-12-17 12:10:27 +0900

    feat:8.txt分割完成

--- screen.py
+++ screen.py
@@ -7,15 +7,15 @@ def GetFileContens(file_name):
         l = list(map(lambda x: x.replace( '\n' , '' ),l))
     return l
 
-def SetFileContens(l,file_name):
-    l = None
-    with open('./mine/data/scenario/input/'+file_name,encoding='utf-8',mode='x') as target:
-        target.writelines(l)
-        # l = list(map(lambda x: x.replace( '\n' , '' ),l))
-    return l
-
-def main():
-    input/
+def SetFileContens(l,file_name="temp"):
+    puts = ["input","output"]
+    input_data = l[:-1:2]
+    output_data = l[1::2]
+    set_data = dict(zip(puts,[input_data,output_data]))
+    for types,data in set_data.items():
+        path = "/".join(['.','mine','data',"scenario",types,file_name])
+        with open(path,encoding='utf-8',mode='w') as target:
+            target.writelines(list(map(lambda x: str(x)+"\n",data)))
 
 def DeleteParentheses(text):
     if "」" in text:
@@ -28,6 +28,10 @@ if __name__ == "__main__":
     file_name = "8uft8.txt"
     file = GetFileContens(file_name)
     text_list = list(map(DeleteParentheses,file))
+    SetFileContens(text_list,file_name)
     # pprint.pprint(text_list)
     # pprint.pprint(list(map(DeleteParentheses,file)))
     # print(DeleteParentheses(file))
+    # print(list([iter(range(2))]*1))
+    # for i in expression_list:
+    #     pass
