4b663cc32f630d8fb4e9a234f4b9b75d3c9481a2
辻気流 <kiryu.18011106@gmail.com>
2021-01-06 05:57:21 +0900

    feat:51,157 data success

--- screen.py
+++ screen.py
@@ -1,8 +1,10 @@
 import pprint
-
+import codecs
+import glob
+import io
 def GetFileContens(file_name):
     l = None
-    with open('./mine/scenario/'+file_name,encoding='utf-8',mode='r') as target:
+    with open(file_name,encoding='utf-8',mode='r') as target:
         l = target.readlines()
         l = list(map(lambda x: x.replace( '\n' , '' ),l))
     return l
@@ -13,7 +15,7 @@ def SetFileContens(l,file_name="temp"):
     output_data = l[1::2]
     set_data = dict(zip(puts,[input_data,output_data]))
     for types,data in set_data.items():
-        path = "/".join(['.','mine','data',"scenario",types,file_name])
+        path = "/".join(['.','mine','data',"scenario",types,file_name.split("\\")[-1]])
         with open(path,encoding='utf-8',mode='w') as target:
             target.writelines(list(map(lambda x: str(x)+"\n",data)))
 
@@ -24,14 +26,45 @@ def DeleteParentheses(text):
         return text[i:j]
     else:
         return False
-if __name__ == "__main__":
-    file_name = "8uft8.txt"
+
+def DeleteSpace(text):
+    return text.split()[-1]
+def EncodeFile():
+    file_list = FileList()
+    for i in file_list:
+        try:
+            temp = i.split("\\")
+            shiftjis_csv_path = i
+            utf8_csv_path = './mine/scenario/utf/'+temp[-1]
+            fin = codecs.open(shiftjis_csv_path, "r",'cp932')
+            fout_utf = codecs.open(utf8_csv_path, "w", "utf-8")
+            for row in fin:
+                fout_utf.write(row)
+            fin.close()
+            fout_utf.close()
+        except Exception as identifier:
+            print(identifier,shiftjis_csv_path)
+def main(file_name):
     file = GetFileContens(file_name)
-    text_list = list(map(DeleteParentheses,file))
-    SetFileContens(text_list,file_name)
+    text_list =[i for i in list(map(DeleteSpace,file)) if i != False]
     # pprint.pprint(text_list)
-    # pprint.pprint(list(map(DeleteParentheses,file)))
-    # print(DeleteParentheses(file))
-    # print(list([iter(range(2))]*1))
-    # for i in expression_list:
-    #     pass
+    SetFileContens(text_list,file_name)
+
+def FileList():
+    files = glob.glob("./mine/scenario/utf/*")
+    result = []
+    for file in files:
+        if "txt" in file:
+            result.append(file)
+            # print(file)
+    return result
+
+
+if __name__ == "__main__":
+    # FileList()
+    # EncodeFile()
+    file_list = FileList()
+    for i in file_list:
+        if "2734" in i:
+            main(i)
+    pass
